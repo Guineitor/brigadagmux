5cc7a8ec3529556d934e927061b1d0e2622ebabc
Guineitor <guineitor.traps236@gmail.com>
2020-09-03 14:37:10 +0000

    added mongoCLient for posts

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/guineitor/brigadagmux
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.4.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+        "context"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+ 	"go.mongodb.org/mongo-driver/bson/primitive"
+        "go.mongodb.org/mongo-driver/mongo"
+        "go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/gorilla/mux"
 )
+
+
+// Client mongo Db
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+    client, err := mongo.Connect(ctx, clientOptions)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    collection = client.Database("posts").Collection("post")
+
+
+}
 
 // P
 type P struct {
@@ -20,6 +49,16 @@ type P struct {
 	Autor     string `json:"autor"`
 	Data      string `json:"data"`
 	Permalink string `json:"permalink"`
+}
+
+type Post struct {
+        Titulo    string             `bson:"titulo"`
+        SubTitulo string             `bson:"sub_titulo"`
+        Conteudo  string             `bson:"conteudo"`
+        Fotos     string             `bson:"fotos"`
+        Autor     string             `bson:"autor"`
+        Data      time.Time 	     `bson:"data"`
+        ID primitive.ObjectID `bson:"permalink"`
 }
 
 // Posts
@@ -64,7 +103,7 @@ func main() {
 	r.HandleFunc("/", Index).Methods("GET")
 	r.HandleFunc("/manifesto", Manifesto).Methods("GET")
 	r.HandleFunc("/blog", Blog).Methods("GET")
-	r.HandleFunc("/post", Post).Methods("GET")
+	r.HandleFunc("/post", GetPost).Methods("GET")
 
 	http.ListenAndServe(":9991", r)
 }
@@ -81,13 +120,19 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 // Blog page
 func Blog(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Print("blog")
+	posts,_ := getAll()
+	for _, v := range posts {
+          fmt.Print(v.Titulo)
+    	}
 	data := GetPosts()
 	blog := template.Must(template.ParseFiles("template/blog.html"))
 	blog.Execute(w, data)
 }
 
 // Post page
-func Post(w http.ResponseWriter, r *http.Request) {
+func GetPost(w http.ResponseWriter, r *http.Request) {
 	// vars := mux.Vars(r)
 	// permalink := vars["permalink"]
 	var p = P{
@@ -99,6 +144,65 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		Data:      "30/06/2020",
 		Permalink: "poder"}
 
+var pp = &Post{
+                Titulo:    "LOREM IPSUM  LOREM IPSUM LOREM IPSUM LOREM IPSUM ",
+                SubTitulo: "LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM ",
+                Conteudo:  "LOREM IPSUM LOREM IPSUM LOREIPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSU ",
+                Fotos:     "static/img/portfolio/fullsize/1.jpg",
+                Autor:     "Comuna da Catarina",
+		ID: primitive.NewObjectID(),
+		Data: time.Now()}
+
+	err := createPost(pp)
+	if err != nil {
+          log.Fatal(err)
+	}
+
 	post := template.Must(template.ParseFiles("template/post.html"))
 	post.Execute(w, p)
+}
+
+func createPost(post *Post) error {
+    _, err := collection.InsertOne(ctx, post)
+  return err
+}
+
+
+func getAll() ([]*Post, error) {
+  // passing bson.D{{}} matches all documents in the collection
+    filter := bson.D{{}}
+    return filterPosts(filter)
+}
+
+func filterPosts(filter interface{}) ([]*Post, error) {
+    // A slice of tasks for storing the decoded documents
+    var posts []*Post
+
+    cur, err := collection.Find(ctx, filter)
+    if err != nil {
+        return posts, err
+    }
+
+    for cur.Next(ctx) {
+        var t Post
+        err := cur.Decode(&t)
+        if err != nil {
+            return posts, err
+        }
+
+        posts = append(posts, &t)
+    }
+
+    if err := cur.Err(); err != nil {
+        return posts, err
+    }
+
+  // once exhausted, close the cursor
+    cur.Close(ctx)
+
+    if len(posts) == 0 {
+        return posts, mongo.ErrNoDocuments
+    }
+
+    return posts, nil
 }
